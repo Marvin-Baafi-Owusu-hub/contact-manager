8614d187029d2505b643dafd62f05fae97b84a3a
Marvin-Baafi-Owusu-hub <owusumarvin77.com>
2026-05-06 15:45:46 +0000

    i have added port 5001

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 connectDb();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5001;
 
 app.use(cors()); 
 app.use(express.json()); 
